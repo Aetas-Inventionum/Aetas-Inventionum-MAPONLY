11929945ef252df497da543795e87d558f701261
felisvonsnuggle <101460250+felisvonsnuggle@users.noreply.github.com>
2025-08-09 18:52:12 +0100

    initial

---
+++ README.md
@@ -0,0 +1,39 @@
+### Github Desktop Install Guide
+0. Make sure that you have "Europa Universalis IV" and "Github Desktop" installed.
+1. Launch the EU4 Launcher and navigate to the "Upload a Mod" button in "All installed mods", then click "Create a Mod". 
+2. In the following panel, name the folder that will be created "Aetas-Inventionum-DEV" (without the quotes). Tags, version, and name do not matter.
+3. Navigate to the folder you just made and move the "descriptor.mod" file elsewhere, do NOT delete it as you will need it again.
+4. Launch Github Desktop and navigate to the "Current repository" button below File/Edit/etc, then click "add". You will be able to select the "Aetas-Inventionum-DEV" repository either in the list on the Github.com tab, or to find it manually by pasting the URL to the repository in to the box on the URL tab. 
+5. In the "Local path" field at the bottom of the "Clone a repository" panel, you should change the path to direct to your EU4 mod folder (not your newly created mod's folder). It will look something like this "C:\Users\USER\Documents\Paradox Interactive\Europa Universalis IV\mod" (without the quotes) but may differ depending on your personal circumstances.
+6. Press "Clone", the blue button at the bottom of the "Clone a repository" panel, and then give the program time to copy the mod's files onto your computer.
+7. Navigate to the folder you made with the EU4 Launcher and move the "descriptor.mod" file back into it, you should now be able to launch the mod.
+
+a. In order to update your install of the mod, you need to press the "Fetch origin" button to the right of the aforementioned "Current repository" button on Github Desktop. If you're a contributor, this same button is used to push commits you've made to the mod and you will need to pull all new changes from origin before you can push.
+
+### Insert Manual install steps here 
+<sup>(Playtesters & Contributors do NOT do this, use above method.)</sup>
+
+--------------------------------------------------------------------------------
+### Credits
+
+Math5615,
+Felis,
+Pleibos,
+
+AdvocatusDiaboli,
+Andermann,
+Dub,
+Good Lookin,
+Harbivore,
+HertogSim,
+Inferno,
+Jabberdrake,
+Jackkade,
+LordCitratus,
+Neothiamin,
+Schockocraft,
+Vjw,
+
+### Other Mods
+
+Typus Orbis Terarrum, Atlas Novum, Beyond Typus, In Fabulae, Cantons for Colonial Flags, Schocko's Outliner Fix, Governments Expanded (for our Byz Government), No More Border Gore!, No Province Flags, Beyond the Cape, The Succession (Original Main Theme).
---
+++ common/defines/AutonomyOverhaulDefines.lua
@@ -0,0 +1,45 @@
+NDefines.NCountry.COUNTRY_DEVELOPMENT_SCALE = 100000	
+NDefines.NGovernment.EXPAND_ADMIN_COST = 50
+NDefines.NAI.GOVERNING_CAPACITY_OVER_PERCENTAGE_TOLERATED = 2 -- Changed from vanilla's 1 to make the ai state over gov cap
+NDefines.NAI.GOVERNING_CAPACITY_REFORM_PROGRESS_GAP_TOLERANCE = 25 -- halved to see if it helps with expanding administration
+NDefines.NAI.GOVERNING_CAPACITY_REFORM_BIAS_IF_NEEDED = 50	-- halved to see if it helps with expanding administration
+NDefines.NAI.ARMY_BUDGET_FRACTION = 0.5
+NDefines.NEconomy.CAPITAL_MAX_AUTONOMY = 100 -- # Harb's Autonomy Overhaul
+
+NDefines.NDiplomacy.AUTONOMY_WARSCORE_COST_MODIFIER = 0.5		-- from 0.33 # Harb's Autonomy Overhaul
+
+NDefines.NDiplomacy.REVANCHISM_MONTHLY_DECAY = 0.277 -- # Harb's Autonomy Overhaul
+
+
+NDefines.NAI.MINIMUM_STRENGTH_TO_AVOID_UNCONDITIONAL_SURRENDER = 30 -- BT increased to hopefully have ai chose to lose this to win another day Harbivore
+NDefines.NAI.UNCONDITIONAL_SURRENDER_HOPELESS_STRENGTH_COMPARISON = 2.5 -- BT decreased to hopefully have ai chose to lose this to win another day Harbivore
+NDefines.NAI.UNCONDITIONAL_SURRENDER_WARSCORE_LIMIT = -50 -- BT decreased to hopefully have ai chose to lose this to win another day Harbivore
+
+NDefines.NEconomy.EDICTS_COST_INCREASE = 1.0				 -- # Harb's Autonomy Overhaul 
+NDefines.NEconomy.EDICTS_DURATION_MONTHS = 0				 -- # Harb's Autonomy Overhaul
+
+	
+NDefines.NGovernment.GOVERNMENT_REFORM_COST_CENTRALIZE_STATE = 0 -- # Harb's Autonomy Overhaul
+NDefines.NGovernment.CENTRALIZE_STATE_ADM_COST = 25 -- # Harb's Autonomy Overhaul
+NDefines.NGovernment.EXPAND_INFRASTRUCTURE_ADM_COST = 50 -- # Harb's Autonomy Overhaul
+
+NDefines.NCountry.REVOLT_SIZE_BASE = 4
+NDefines.NCountry.REVOLT_SIZE_DEVELOPMENT_MULTIPLIER = 0.08
+NDefines.NCountry.STATE_MAINTENANCE_DEV_FACTOR = 0.006			-- from 0p007  -- # Harb's Autonomy Overhaul
+
+
+NDefines.NMilitary.LOOT_DEVASTATION_IMPACT = 20						-- how much devastation full loot impacts
+NDefines.NMilitary.SCORCHED_DEVASTATION_IMPACT = 20 					-- how much devastation scorching impacts.
+NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.3	 -- # Harb's Autonomy Overhaul
+
+NDefines.NCountry.STARTING_ARMY_SIZE = 0.5							 -- # Harb's Autonomy Overhaul
+NDefines.NCountry.STARTING_ARMY_SIZE_AT_WAR = 0.75					 -- # Harb's Autonomy Overhaul
+NDefines.NCountry.STARTING_FLEET_SIZE = 0.75					-- # Harb's Autonomy Overhaul
+
+NDefines.NCountry.AUTONOMY_AT_DIPLO_ANNEX = 60					-- Autonomy added when diplo-annexing
+NDefines.NCountry.AUTONOMY_AT_CONQUEST = 80						-- Autonomy added at conquest
+NDefines.NCountry.AUTONOMY_AT_CONQUEST_CLAIM = 80				-- Autonomy added at conquest if you have a claim
+
+
+NDefines.NGovernment.GOVERNMENT_REFORM_YEARLY_BASE_PROGRESS = 8
+
